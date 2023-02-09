4f4ede25eb4e4301b849d89419b9e63ea5f94a20
Lawrence Artl <lorenzo.joseph.dominguez@gmail.com>
2023-02-09 12:24:20 -0800

    added custom button script

--- PanIQVideoPlayer_V2/Server/ClientMasterForm.Designer.cs
+++ PanIQVideoPlayer_V2/Server/ClientMasterForm.Designer.cs
@@ -42,18 +42,20 @@
             // 
             // listMessages
             // 
-            this.listMessages.Location = new System.Drawing.Point(167, 96);
+            this.listMessages.Location = new System.Drawing.Point(84, 50);
+            this.listMessages.Margin = new System.Windows.Forms.Padding(2);
             this.listMessages.Multiline = true;
             this.listMessages.Name = "listMessages";
             this.listMessages.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.listMessages.Size = new System.Drawing.Size(818, 434);
+            this.listMessages.Size = new System.Drawing.Size(411, 228);
             this.listMessages.TabIndex = 13;
             // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(650, 610);
+            this.btnSend.Location = new System.Drawing.Point(325, 317);
+            this.btnSend.Margin = new System.Windows.Forms.Padding(2);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(159, 81);
+            this.btnSend.Size = new System.Drawing.Size(80, 42);
             this.btnSend.TabIndex = 12;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = true;
@@ -61,9 +63,10 @@
             // 
             // btnConnect
             // 
-            this.btnConnect.Location = new System.Drawing.Point(826, 610);
+            this.btnConnect.Location = new System.Drawing.Point(413, 317);
+            this.btnConnect.Margin = new System.Windows.Forms.Padding(2);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(159, 81);
+            this.btnConnect.Size = new System.Drawing.Size(80, 42);
             this.btnConnect.TabIndex = 11;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = true;
@@ -71,59 +74,65 @@
             // 
             // textMessage
             // 
-            this.textMessage.Location = new System.Drawing.Point(167, 554);
+            this.textMessage.Location = new System.Drawing.Point(84, 288);
+            this.textMessage.Margin = new System.Windows.Forms.Padding(2);
             this.textMessage.Name = "textMessage";
-            this.textMessage.Size = new System.Drawing.Size(818, 31);
+            this.textMessage.Size = new System.Drawing.Size(411, 20);
             this.textMessage.TabIndex = 10;
             // 
             // textServerIp
             // 
-            this.textServerIp.Location = new System.Drawing.Point(167, 43);
+            this.textServerIp.Location = new System.Drawing.Point(84, 22);
+            this.textServerIp.Margin = new System.Windows.Forms.Padding(2);
             this.textServerIp.Name = "textServerIp";
-            this.textServerIp.Size = new System.Drawing.Size(818, 31);
+            this.textServerIp.Size = new System.Drawing.Size(411, 20);
             this.textServerIp.TabIndex = 9;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(31, 560);
+            this.label2.Location = new System.Drawing.Point(16, 291);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(106, 25);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 8;
             this.label2.Text = "Message:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(56, 43);
+            this.label1.Location = new System.Drawing.Point(28, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.Size = new System.Drawing.Size(41, 13);
             this.label1.TabIndex = 7;
             this.label1.Text = "Server:";
             // 
             // listClient
             // 
             this.listClient.FormattingEnabled = true;
-            this.listClient.ItemHeight = 25;
-            this.listClient.Location = new System.Drawing.Point(1007, 149);
+            this.listClient.Location = new System.Drawing.Point(504, 77);
+            this.listClient.Margin = new System.Windows.Forms.Padding(2);
             this.listClient.Name = "listClient";
-            this.listClient.Size = new System.Drawing.Size(232, 529);
+            this.listClient.Size = new System.Drawing.Size(118, 277);
             this.listClient.TabIndex = 14;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(1002, 49);
+            this.label3.Location = new System.Drawing.Point(501, 25);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(73, 25);
+            this.label3.Size = new System.Drawing.Size(36, 13);
             this.label3.TabIndex = 15;
             this.label3.Text = "Client:";
             // 
             // btnCommand
             // 
-            this.btnCommand.Location = new System.Drawing.Point(1007, 84);
+            this.btnCommand.Location = new System.Drawing.Point(504, 44);
+            this.btnCommand.Margin = new System.Windows.Forms.Padding(2);
             this.btnCommand.Name = "btnCommand";
-            this.btnCommand.Size = new System.Drawing.Size(232, 47);
+            this.btnCommand.Size = new System.Drawing.Size(116, 24);
             this.btnCommand.TabIndex = 16;
             this.btnCommand.Text = "Refresh";
             this.btnCommand.UseVisualStyleBackColor = true;
@@ -131,9 +140,9 @@
             // 
             // ClientMasterForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1273, 725);
+            this.ClientSize = new System.Drawing.Size(642, 394);
             this.Controls.Add(this.btnCommand);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.listClient);
@@ -144,8 +153,9 @@
             this.Controls.Add(this.textServerIp);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.MaximumSize = new System.Drawing.Size(1299, 796);
-            this.MinimumSize = new System.Drawing.Size(1299, 796);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximumSize = new System.Drawing.Size(658, 433);
+            this.MinimumSize = new System.Drawing.Size(658, 433);
             this.Name = "ClientMasterForm";
             this.Text = "Client Master";
             this.Load += new System.EventHandler(this.ClientMasterForm_Load);
